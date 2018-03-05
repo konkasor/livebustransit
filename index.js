9d55b8b05f3c87cea71f41210e2697646fc27eef
AdrianMihai <adrian_cs98@yahoo.co.uk>
2018-03-05 23:42:11 +0200

    Add mysql database connection.

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ const express = require('express'),
 	port = process.env.PORT || 5000,
 	hostName = 'localhost';
 
-/*var con = mySql.createConnection({
-  host: hostName,
-  user: "root",
-  password: "ilikeit2",
+var con = mySql.createConnection({
+  host: "sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "zjkmgixo5uc7x674",
+  password: "q8g7itf4lyccen2a",
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-*/
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
-
 
 app.get('/', (req, res) => {
 	res.type('text/html');
